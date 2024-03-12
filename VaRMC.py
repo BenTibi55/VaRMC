b702cc908ba4bf99384eb52577b20e6117bac5f3
Benjamin Tibi <163139573+BenTibi55@users.noreply.github.com>
2024-03-12 16:02:39 +0100

    Create VaRMC.py

---
+++ VaRMC.py
@@ -0,0 +1,73 @@
+#Monte Carlo simulation
+import pandas as pd
+import numpy as np
+import datetime as dt
+from pandas_datareader import data as pdr
+import matplotlib.pyplot as plt
+
+# Import data
+def getData(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start, end)
+    stockData = stockData['Close']
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
+
+stockList = ['CBA', 'BHP', 'TLS', 'NAB', 'WBC', 'STO']
+stocks = [stock+'.AX' for stock in stockList]
+endDate = dt.datetime.now()
+startDate = endDate - dt.timedelta(days=800)
+
+meanReturns, covMatrix = getData(stocks,startDate, endDate)
+returns = returns.dropna()
+
+weights = np.random.random(len(meanReturns))
+weights /= np.sum(weights)
+
+mc_sims = 400 # number of simulations
+T = 100 #timeframe in days
+
+meanM = np.full(shape=(T, len(weights)), fill_value=meanReturns)
+meanM = meanM.T
+
+portfolio_sims = np.full(shape=(T, mc_sims), fill_value=0.0)
+
+initialPortfolio = 10000
+
+for m in range(0, mc_sims):
+    # MC loops
+    Z = np.random.normal(size=(T, len(weights)))
+    L = np.linalg.cholesky(covMatrix)
+    dailyReturns = meanM + np.inner(L, Z)
+    portfolio_sims[:,m] = np.cumprod(np.inner(weights, dailyReturns.T)+1)*initialPortfolio
+plt.plot(portfolio_sims)
+plt.ylabel('Portfolio Value ($)')
+plt.xlabel('Days')
+plt.title('MC simulation of a stock portfolio')
+plt.show()
+
+def mcVaR(returns, alpha=5):
+    """ Input: pandas series of returns
+        Output: percentile on return distribution to a given confidence level alpha
+    """
+    if isinstance(returns, pd.Series):
+        return np.percentile(returns, alpha)
+    else:
+        raise TypeError("Expected a pandas data series.")
+portResults = pd.Series(portfolio_sims[-1,:])
+
+VaR = initialPortfolio - mcVaR(portResults, alpha=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
